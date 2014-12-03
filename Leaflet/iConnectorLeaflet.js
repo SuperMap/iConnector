0ede39d109a693129fa1808eaa5dca607f1c8e72
pasu <super123>
2014-12-03 14:55:11 +0800

    leaflet支持4326投影系

--- Leaflet/iConnectorLeaflet.js
+++ Leaflet/iConnectorLeaflet.js
@@ -25,12 +25,13 @@ SuperMap.Web.iConnector.Leaflet = function(){
 }
 /**
  *  APIMethod:
- *  创建Leaflet的图层L.tileLayer.canvas，这里的图层中切片的来源为iserver服务器（只能是3857的地图）
+ *  创建Leaflet的图层L.tileLayer.canvas，这里的图层中切片的来源为iserver服务器（支持3857的地图和4326的地图）
  * @param url  {String}  地图服务的url地址，如：“http://localhost:8090/iserver/services/map-china400/rest/maps/China”
  * @param options 可选的参数
  * transparent - {Boolean} 设置切片是否透明，默认为true
  * cacheEnabled - {Boolean} 设置是否使用缓存，默认为false
  * layersID - {String} 设置临时图层的id，一般用于专题图的叠加使用
+ * projection-{String}设置图层的投影系，可设置为"3857"或者"4326"，默认为"3857"
  * @returns {Object} 返回Leaflet的扩展图层对象
  */
 SuperMap.Web.iConnector.Leaflet.getLayer = function(url,options){
@@ -49,7 +50,7 @@ SuperMap.Web.iConnector.Leaflet.getLayer = function(url,options){
     }
     layerUrl += "&transparent=" + transparent;
 
-    //是否是否使用缓存
+    //是否使用缓存
     var cacheEnabled = false;
     if(options && options.cacheEnabled !=undefined)
     {
@@ -62,38 +63,68 @@ SuperMap.Web.iConnector.Leaflet.getLayer = function(url,options){
     {
         layerUrl += "&layersID=" +options.layersID;
     }
+    //如果有projection，并且只能是4326或者3857的地图。
+    var projection="3857";
+    if(options&&options.projection){
+           if(options.projection==="4326"){
+               projection="4326";
+           }
+    }
+    layerUrl+="&projection="+projection;
+    //计算分辨率和比例尺
     var resLen = 17;
     var resStart = 0;
-    layer.resolutions3857 = [];
-    var dpi = 95.99999999999984;
-    layer.scales3857 = [];
-    for(var i=resStart;i<=resLen;i++){
-        var res3857 = 156543.0339/Math.pow(2,i);
-        layer.resolutions3857.push(res3857);
+    var resolutions=[];
 
-        var scale3857 = 0.0254/dpi/res3857;
-        layer.scales3857.push(scale3857);
-    }
+    var dpi = 95.99999999999984;
+
+    //layer.scales3857 = [];
+    var scales=[];
+     if(projection==="3857"){
+           for(var i=resStart;i<=resLen;i++){
+                   var res3857 = 156543.0339/Math.pow(2,i);
+                   // layer.resolutions3857.push(res3857);
+               resolutions.push(res3857);
+
+                   var scale3857 = 0.0254/dpi/res3857;
+                   // layer.scales3857.push(scale3857);
+                  scales.push(scale3857);
+           }
+           layer.scales=scales;
+     }
+     else{
+           for(var i=resStart;i<=resLen;i++){
+                var res4326 = 1.40625/Math.pow(2,i);
+               resolutions.push(res4326);
+
+                var scale4326 = 0.0254*360/dpi/res4326/Math.PI/2/6378137;
+                scales.push(scale4326);
+           }
+         layer.scales=scales;
+     }
+
     layer.url = layerUrl;
 
     layer.drawTile = function(canvas, tilePoint, zoom){
+
         var ctx = canvas.getContext('2d');
-        var po = Math.pow(2,zoom);
         var x = tilePoint.x;
         var y = tilePoint.y;
+        var po = Math.pow(2,zoom);
+
         x-=po/2;
         y=po/2-y-1;
         //使用bounds出图（也可以使用center）
-        var left = x*256*this.resolutions3857[zoom];
-        var bottom = y*256*this.resolutions3857[zoom];
-        var right = (x + 1)*256*this.resolutions3857[zoom];
-        var top = (y + 1)*256*this.resolutions3857[zoom];
+        var left = x*256*resolutions[ zoom];
+        var bottom = y*256*resolutions[zoom];
+        var right = (x + 1)*256*resolutions[zoom];
+        var top = (y + 1)*256*resolutions[zoom];
         //将bounds组合到url里面
         tileUrl =this.url + "&viewBounds=" +"{\"leftBottom\" : {\"x\":" + left +",\"y\":" + bottom +"},\"rightTop\" : {\"x\":" + right +",\"y\":" +top + "}}";
 
-        tileUrl +="&scale=" +this.scales3857[zoom];
-        tileUrl += "&prjCoordSys={\"epsgCode\":3857}";
-
+        tileUrl +="&scale=" +scales[zoom];
+        var epsg=projection==="3857"?3857:4326;
+        tileUrl += "&prjCoordSys={\"epsgCode\":"+epsg+"}";
         this.preImage(tileUrl,function(){
             ctx.drawImage(this,0,0,256,256);
         });
