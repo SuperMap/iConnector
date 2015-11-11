076d9c4174e9c532106fb5991b1b93a9b133b379
vein <trojan_ann@aliyun.com>
2015-11-11 09:37:12 +0800

    升级OL3库为3.10.1

--- OpenLayers3/iConnectorOpenLayers3.js
+++ OpenLayers3/iConnectorOpenLayers3.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: CC
- * Date: 15-3-3
- * Time: 下午1:39
- * To change this template use File | Settings | File Templates.
- */
 //判定一下是否存在了SuperMap.Web，如果没有则初始化一个
 if(SuperMap.Web == undefined )
 {
@@ -81,59 +74,58 @@ SuperMap.Web.iConnector.OpenLayers3.getLayer = function(url,options){
     var resolutions3857=[];
     var resolutions4326=[];
 
-        for(var i=resStart;i<=resLen;i++){
-            var res3857 = 156543.0339/Math.pow(2,i);
+    for(var i=resStart;i<=resLen;i++){
+        var res3857 = 156543.0339/Math.pow(2,i);
 
-            resolutions3857.push(res3857);
+        resolutions3857.push(res3857);
 
-            var scale3857 = 0.0254/dpi/res3857;
-            scales3857.push(scale3857);
-        }
-        tileLayer.scales=scales3857;
+        var scale3857 = 0.0254/dpi/res3857;
+        scales3857.push(scale3857);
+    }
+    tileLayer.scales=scales3857;
 
 
-        for(var i=resStart;i<=resLen;i++){
-            var res4326 = 1.40625/Math.pow(2,i);
-            resolutions4326.push(res4326);
+    for(var i=resStart;i<=resLen;i++){
+        var res4326 = 1.40625/Math.pow(2,i);
+        resolutions4326.push(res4326);
 
-            var scale4326 = 0.0254*360/dpi/res4326/Math.PI/2/6378137;
-            scales4326.push(scale4326);
-        }
-        tileLayer.scales=scales4326;
+        var scale4326 = 0.0254*360/dpi/res4326/Math.PI/2/6378137;
+        scales4326.push(scale4326);
+    }
+    tileLayer.scales=scales4326;
 
     function getSource(){
 
         var tileUrl;
         var source= new ol.source.TileImage({
-              tileUrlFunction:function(tileCoord, pixelRatio, projection){
-                  var z=tileCoord[0], x= tileCoord[1],y =tileCoord[2];
-                  if(pro==="3857"){
-                      var po = Math.pow(2,z-1);
-                      x-=po;
-                      y-=po;
-                      var left = x*256*resolutions3857[z];
-                      var bottom = y*256*resolutions3857[z];
-                      var right = (x + 1)*256*resolutions3857[z];
-                      var top = (y+1)*256*resolutions3857[z];
-                      tileUrl  = layerUrl+"&viewBounds=" +"{\"leftBottom\" : {\"x\":" + left +",\"y\":" + bottom +"},\"rightTop\" : {\"x\":" + right +",\"y\":" +top + "}}";
-                      tileUrl +="&scale=" +scales3857[z];
+            tileUrlFunction:function(tileCoord, pixelRatio, projection){
+                var z=tileCoord[0], x= tileCoord[1],y =tileCoord[2];
+                if(pro==="3857"){
+                    x-= Math.pow(2,z-1);
+                    y+=Math.pow(2,z-1);
+                    var left = x*256*resolutions3857[z];
+                    var bottom = y*256*resolutions3857[z];
+                    var right = (x + 1)*256*resolutions3857[z];
+                    var top = (y+1)*256*resolutions3857[z];
+                    tileUrl  = layerUrl+"&viewBounds=" +"{\"leftBottom\" : {\"x\":" + left +",\"y\":" + bottom +"},\"rightTop\" : {\"x\":" + right +",\"y\":" +top + "}}";
+                    tileUrl +="&scale=" +scales3857[z];
 
-                  }
-                  else if(pro==="4326"){
-                      x-=Math.pow(2,z-1);
-                      y-= Math.pow(2,z-2);
-                      var left = x*256*resolutions4326[z];
-                      var bottom = y*256*resolutions4326[z];
-                      var right = (x + 1)*256*resolutions4326[z];
-                      var top = (y+1)*256*resolutions4326[z];
-                      tileUrl  = layerUrl+"&viewBounds=" +"{\"leftBottom\" : {\"x\":" + left +",\"y\":" + bottom +"},\"rightTop\" : {\"x\":" + right +",\"y\":" +top + "}}";
-                      tileUrl +="&scale=" +scales4326[z];
+                }
+                else if(pro==="4326"){
+                    x-= Math.pow(2,z-1);
+                    y+=Math.pow(2,z-1);
+                    var left = x*256*resolutions4326[z];
+                    var bottom = y*256*resolutions4326[z];
+                    var right = (x + 1)*256*resolutions4326[z];
+                    var top = (y+1)*256*resolutions4326[z];
+                    tileUrl  = layerUrl+"&viewBounds=" +"{\"leftBottom\" : {\"x\":" + left +",\"y\":" + bottom +"},\"rightTop\" : {\"x\":" + right +",\"y\":" +top + "}}";
+                    tileUrl +="&scale=" +scales4326[z];
 
-                  }
-                  var epsg=pro==="3857"?3857:4326;
-                  tileUrl += "&prjCoordSys={\"epsgCode\":"+epsg+"}";
-             return tileUrl;
-              }
+                }
+                var epsg=pro==="3857"?3857:4326;
+                tileUrl += "&prjCoordSys={\"epsgCode\":"+epsg+"}";
+                return tileUrl;
+            }
         });
         return  source;
     }
@@ -223,7 +215,7 @@ SuperMap.Web.iConnector.OpenLayers3.transferLine = function(array,projection){
             //支持supermap的LineString
             if(array[i].CLASS_NAME && array[i].CLASS_NAME == "SuperMap.Geometry.LineString")
             {
-             var points = SuperMap.Web.iConnector.OpenLayers3.transferPoint(array[i].components,pro);
+                var points = SuperMap.Web.iConnector.OpenLayers3.transferPoint(array[i].components,pro);
 
                 line = new ol.geom.LineString(getcoordinate(points));
             }
@@ -240,7 +232,7 @@ SuperMap.Web.iConnector.OpenLayers3.transferLine = function(array,projection){
 function getcoordinate(points){
     var coordinates=[];
     for(var i=0;i<points.length;i++){
-         coordinates.push(points[i].getCoordinates());
+        coordinates.push(points[i].getCoordinates());
     }
     return coordinates;
 }
